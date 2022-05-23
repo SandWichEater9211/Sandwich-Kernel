38f4565b00333f127afb0e6e98806f1341dec0d8
moin <ahmadkhan03122000@gmail.com>
2022-05-23 17:09:46 +0530

    Revert "cpufreq: flush queued work before freeing policy"
    This reverts commit 9df35ec6ec995c0016a45a26a3d381119eff265f.

--- drivers/cpufreq/cpufreq.c
+++ drivers/cpufreq/cpufreq.c
@@ -1164,19 +1164,8 @@ static int __cpufreq_remove_dev(struct device *dev, struct subsys_interface *sif
 		wait_for_completion(cmp);
 		pr_debug("wait complete\n");
 
-		/* Remove policy from list of active policies */
-		write_lock_irqsave(&cpufreq_driver_lock, flags);
-		list_del(&policy->policy_list);
-		write_unlock_irqrestore(&cpufreq_driver_lock, flags);
-
-		if (!cpufreq_suspended) {
-			flush_work(&policy->update);
-			cpufreq_policy_free(policy);
-		}
-	} else if (has_target()) {
-		ret = __cpufreq_governor(policy, CPUFREQ_GOV_START);
-		if (!ret)
-			ret = __cpufreq_governor(policy, CPUFREQ_GOV_LIMITS);
+		if (cpufreq_driver->exit)
+			cpufreq_driver->exit(data);
 
 		free_cpumask_var(data->related_cpus);
 		free_cpumask_var(data->cpus);
